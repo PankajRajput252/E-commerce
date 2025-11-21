fac09c48dd330ec43b1c8f76266e3350b6a10734
Pankaj Rajput <Pankaj.singh@tapplent.com>
2025-11-21 22:39:38 +0530

    permission

--- src/main/java/com/mineCryptos/config/SecurityConfig.java
+++ src/main/java/com/mineCryptos/config/SecurityConfig.java
@@ -42,6 +42,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.csrf().disable()
                 .authorizeHttpRequests()
                 .antMatchers("/api/v1/auth/**").permitAll()
+                .antMatchers("/api/deposit/webhook").permitAll()
                 .antMatchers(HttpMethod.GET).permitAll()
                 .anyRequest().
                 authenticated()
